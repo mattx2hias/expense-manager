97e25a3c6e01d3742cec8416bd57c5ac54a50f73
qpco <qpco@pm.me>
2022-04-26 13:24:57 -0500

    add: generateID util

---
+++ src/test/java/dev/matthias/utiltests/GenerateIDTest.java
@@ -0,0 +1,15 @@
+package dev.matthias.utiltests;
+
+import dev.matthias.utilities.GenerateID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class GenerateIDTest {
+
+    @Test
+    @DisplayName("Should generate unique random 4 digit number")
+    void shouldGenerateUniqueRandom4DigitNumber() {
+        Assertions.assertNotEquals(GenerateID.generateRandomID(), GenerateID.generateRandomID());
+    }
+}
